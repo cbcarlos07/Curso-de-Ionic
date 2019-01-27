563b0b744761eac98279c4df95cf42f4e7419afd
Carlos Bruno <cbcarlos08@gmail.com>
2019-01-27 18:58:33 -0400

    Tables

--- api_ionic/database/migrations/2014_10_12_000000_create_users_table.php
+++ api_ionic/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',191);
-            $table->string('email',191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email',191);
             $table->string('password',191);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
